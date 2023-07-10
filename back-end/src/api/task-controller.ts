d459d39eba9e8e84b5b986f36d444be5ab229b79
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-07-10 08:18:46 +0530

    Create connection pool

--- back-end/src/api/task-controller.ts
+++ back-end/src/api/task-controller.ts
@@ -1,4 +1,20 @@
 import express from "express";
+import dotenv from 'dotenv';
+import mysql, {Pool} from "promise-mysql";
 
 export const router = express.Router();
 
+let pool:Pool;
+dotenv.config();
+
+// IIFE - Immediately Invoked Function Expression
+(async ()=>{
+    pool =  await mysql.createPool({
+        host: process.env.host,
+        port: +process.env.port!,
+        database: process.env.database,
+        user: process.env.username,
+        password: process.env.password,
+        connectionLimit: +process.env.connection_limit!,
+    });
+})();
